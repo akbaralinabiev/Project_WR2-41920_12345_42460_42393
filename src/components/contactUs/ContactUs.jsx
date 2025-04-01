203e4364356ffbe502c353a863605d357764d477
Akbarali Nabiev <akbaralinabiev>
2025-04-01 10:01:39 +0200

    updating components

---
+++ src/components/contactUs/ContactUs.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./contactUs.css";
+
+
+import Contact from "../../components/buttons/desktopButtons/contactUsButton/ContactUsButton.jsx";
+import BrowsePackages from "../../components/buttons/desktopButtons/browsePackages/BrowsePackages.jsx";
+import { default as Message } from "../../assets/images/service-images/Message.svg";
+import { default as Packages } from "../../assets/images/service-images/Packages.svg";
+
+
+export const ContactUs = () => {
+
+     return (
+       <div id="#contact" className="contact">
+         <div className="contact-process">
+           <div className="contact-process_header">
+             <h5>Let’s get in touch</h5>
+             <h1>Ready to start working together with us ?</h1>
+           </div>
+
+           <div className="contact-process_container">
+             <div className="contact-process_container-box">
+               <img src={Message} alt="" />
+               <div className="contact-process_container-box-text">
+                 <h3>Send us a message!</h3>
+                 <p>
+                   Have questions or want to discuss your project in detail?
+                   Send us a message, and our team will get back to you shortly
+                   to kickstart the conversation.
+                 </p>
+                 <Contact />
+               </div>
+             </div>
+             <div className="contact-process_container-box">
+               <img src={Packages} alt="" />
+               <div className="contact-process_container-box-text">
+                 <h3>Browse our packages</h3>
+                 <p>
+                   Looking for predefined service packages tailored to your
+                   needs? Explore our packages to find the perfect solution for
+                   your project.
+                 </p>
+                 <BrowsePackages />
+               </div>
+             </div>
+           </div>
+           <span className="contact-spacer"></span>
+         </div>
+       </div>
+     );
+}
+
+export default ContactUs;
---
+++ src/divider/Divider.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './divider.css';
+
+export const Divider = () => {
+     return (
+       <div className="divider">
+         <div className="divider-line"></div>
+       </div>
+     );
+
+}
+
+export default Divider;
