c8fc350ca9a07352f337fbad3cedbdc951748662
ufukemre0002 <producedbycontact@gmail.com>
2025-04-22 03:58:14 +0000

    just created the not found page

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -5,27 +5,31 @@ import "./error.css";
 
 // imported Components
 import { default as Navbar } from "../../components/navbar/Navbar";
+import { Footer} from "../../components/export.js";
 
 const ErrorPage = () => {
 
      return (
-          <div className="error">
+          <div>
+               <div className="error">
                <Navbar />
-               <div>
+               <div className="error-header">
                     <span>404</span>
                     <h1>Page not found</h1>
                     <h3>The page you are looking for doesn't exist or have been moved.
                     </h3>
                     <div className="error-header-btns">
-                         <Link to="/ ">
+                         <Link className="back-to-home-btn" to="/ ">
                               Go Home
                          </Link>
                          <h4>or</h4>
-                         <Link to="/packages ">
+                         <Link className="back-to-packages-btn" to="/packages ">
                               Go to Packages
                          </Link>
                     </div>
                </div>
+               </div>
+               <Footer />
           </div>
      );
 }
