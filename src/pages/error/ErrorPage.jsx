1a53d1df4a1b40c6723bbbe628923ee8ea3c9717
ufukemre0002 <producedbycontact@gmail.com>
2025-04-22 02:54:00 +0000

    creating the errot page

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./error.css";
 
 
@@ -8,9 +9,23 @@ import { default as Navbar } from "../../components/navbar/Navbar";
 const ErrorPage = () => {
 
      return (
-          <div>
+          <div className="error">
                <Navbar />
-               <h1>ErrorPage</h1>
+               <div>
+                    <span>404</span>
+                    <h1>Page not found</h1>
+                    <h3>The page you are looking for doesn't exist or have been moved.
+                    </h3>
+                    <div className="error-header-btns">
+                         <Link to="/ ">
+                              Go Home
+                         </Link>
+                         <h4>or</h4>
+                         <Link to="/packages ">
+                              Go to Packages
+                         </Link>
+                    </div>
+               </div>
           </div>
      );
 }
